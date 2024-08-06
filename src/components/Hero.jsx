90c4000dfdee7ef676b6591a2f2396281cb02991
Majahangir <mariam.jahangir25@gmail.com>
2024-08-06 15:58:25 +0500

    FINAL

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,17 +7,16 @@ const Hero=()=>{
             <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p> 
             <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Grow with data</h1>  
             <div className="flex justify-center text-center pr-4 py-4 ">
-                <p className="md:text-5xl sm:text-4xl text-xl font-bold ">
+                <p className="md:text-4xl sm:text-2xl text-xl font-bold ">
                     fast, flexible financing for
                     </p>
               <ReactTyped 
-              className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
+              className="md:text-4xl sm:text-2xl text-xl font-bold md:pl-4 pl-2"
               strings={['BTB','BTC','SAAS']} typeSpeed={120} backSpeed={140} loop/>
              
                 </div>
 
-              
-               
+
              <p className="md:text-2xl text-xl font-bold text-gray-500 ">Monitor your data analytics to increase revenue for BTB,BTC & SAAS platforms</p> 
              <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black ">Get Started</button>
             </div>
